e630350bf695f802c437312cf6ca46595ab8c168
Carlos Fraga <cfraga@salsify.com>
2023-12-10 22:45:47 +0000

    day9 pt 2

--- src/day9/solve.rs
+++ src/day9/solve.rs
@@ -40,34 +40,41 @@ fn calculate_sequence(seq: Vec<i128>) -> i128 {
     .fold(0, |acc, v| acc + v)
 }
 
+fn calc_sequence_first(seq: Vec<i128>) -> i128 {
+    let mut acc = 0;
+    let mut executions = vec![];
+    executions.push(seq);
+    loop {
+         executions.push(seq_differences(executions.last().unwrap()));
+ 
+         if executions.last().unwrap().iter().all(|n| *n == 0i128) {
+             break;
+         }
+    }
+
+   executions.iter()
+    .inspect(|v| debug!("vals {:?}", v)) 
+    .map(|vals| vals.first().unwrap())
+    .rev()
+    .fold(0, |acc, v| { debug!("\t{}", acc); v - acc } )
+}
+
 fn seq_differences(seq: &Vec<i128>) -> Vec<i128> {
     seq.windows(2).map(|ab| ab[1] - ab[0]).collect()
 }
 
-pub fn part2(file_path: String) -> u64 {
+pub fn part2(file_path: String) -> i128 {
 
     let f = File::open(file_path).expect("couldnt open file");
     let reader = BufReader::new(f);
 
-    todo!();
+    reader.lines()
+    .flat_map( |maybe_l| maybe_l.ok())
+    .map(|l| l.split_whitespace().map(|n| n.parse::<i128>().unwrap()).collect())
+    .map(|sequence| calc_sequence_first(sequence))
+    .inspect(|val| debug!("total {}", val))
+    .sum()
 }
-
-pub fn lcm(nums: &[u128]) -> u128 {
-    if nums.len() == 1 {
-        return nums[0];
-    }
-    let a = nums[0];
-    let b = lcm(&nums[1..]);
-    a * b / gcd_of_two_numbers(a, b)
-}
-
-fn gcd_of_two_numbers(a: u128, b: u128) -> u128 {
-    if b == 0 {
-        return a;
-    }
-    gcd_of_two_numbers(b, a % b)
-}
-
 
 
 
@@ -88,30 +95,30 @@ mod tests {
     #[test]
     pub fn test_input_1(){
         let test_file = format!("src/{}/input", DAY).to_string();
-        let expected: i128 = 2;
+        let expected: i128 = 1953784198;
 
         let result = part1(test_file);
 
         assert_eq!(result, expected);
     }
 
-    // #[test]
-    // pub fn test_sample_2(){
-    //     let test_file = format!("src/{}/sample_2", DAY).to_string();
-    //     let expected = 6;
+    #[test]
+    pub fn test_sample_2(){
+        let test_file = format!("src/{}/sample_1", DAY).to_string();
+        let expected: i128 = 2;
 
-    //     let result = part2(test_file);
+        let result = part2(test_file);
 
-    //     assert_eq!(result, expected);
-    // }
+        assert_eq!(result, expected);
+    }
 
-    // #[test]
-    // pub fn test_input_2(){
-    //     let test_file = format!("src/{}/input", DAY).to_string();
-    //     let expected: u64 = 2;
+    #[test]
+    pub fn test_input_2(){
+        let test_file = format!("src/{}/input", DAY).to_string();
+        let expected: i128 = 957;
 
-    //     let result = part2(test_file);
+        let result = part2(test_file);
 
-    //     assert_eq!(result, expected);
-    // }
+        assert_eq!(result, expected);
+    }
 }
